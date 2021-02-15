657e5b26bff02f993b6e7f14602e3f14f29b2aed
Vjacheslav Borisov <slavb@ilb.ru>
2021-02-15 12:06:43 +0400

    fix root test

--- src/User.js
+++ src/User.js
@@ -1,6 +1,10 @@
+import posix from 'posix';
+
 export default class User {
   constructor(uid) {
     this.uid = uid;
+    const entry = posix.getpwnam(uid);
+    this.name = entry.gecos;
   }
 
   /*eslint no-unused-vars: ["error", { "args": "none" }]*/
--- src/__tests__/User.test.js
+++ src/__tests__/User.test.js
@@ -1,6 +1,11 @@
 import User from '../User';
 
+const root = new User('root');
 const user = new User(process.env.USER);
+
+test('root name is root', () => {
+  expect(root.name).toStrictEqual('root');
+});
 test('contains group', () => {
   expect(user.hasGroup(process.env.USER)).toStrictEqual(true);
 });
