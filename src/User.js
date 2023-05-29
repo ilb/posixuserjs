499fe830b4400a624402754b15edf05f7f8b48bc
MaximumQuiet <contact@maximumquiet.cyou>
2023-05-29 10:24:18 +0400

    Misc: add debug information

--- src/User.js
+++ src/User.js
@@ -1,16 +1,24 @@
+import createDebug from 'debug';
 import posix from '@maximumquiet/posix';
+
+const debug = createDebug('posixuserjs');
 
 export default class User {
   constructor(code) {
     this.entry = posix.getpwnam(code);
+    debug('gathered user by getpwnam: %s', this.entry.name);
   }
 
   hasGroup(groupName) {
     try {
       const groupInfo = posix.getgrnam(groupName);
-
+      debug('gathered group by getgrnam: %o,', {
+        name: groupInfo.name,
+        members: groupInfo.members
+      });
       return groupInfo.members.indexOf(this.entry.name) !== -1;
     } catch (err) {
+      console.trace(err);
       return false;
     }
   }
